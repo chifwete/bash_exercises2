826fcda0f7e2cd840b3ef8c377f1c16f2685ad9f
chifwete <you@example.com>
2020-10-07 15:01:59 +0300

    initial commit

---
+++ scriptFolder/script.sh
@@ -0,0 +1,22 @@
+ #! /usr/bin/bash
+
+# creating a directory
+mkdir TestDir1
+
+#make TestDir the working directory
+cd TestDir1
+
+#make  directories 0-9 in TestDir
+for i in {0..9}
+do
+   mkdir dir${i}
+cd dir${i}
+#make file.txt in directories 0-9
+
+for j in {0..9}
+do
+   touch file${i}${j}.txt
+
+ done
+   cd ..
+done
